5e20bf2da3bd0ac470862d97def1136af2d3c058
meyiao <jinaqiaoqiao@gmail.com>
2017-02-13 16:11:04 +0800

    EKF-SLAM

---
+++ ekfslam/build/CMakeFiles/aslam.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/aslam.dir/src/main.cpp.o"
+  "CMakeFiles/aslam.dir/src/Camera.cpp.o"
+  "CMakeFiles/aslam.dir/src/CamPose.cpp.o"
+  "CMakeFiles/aslam.dir/src/Feature.cpp.o"
+  "CMakeFiles/aslam.dir/src/EKFSystem.cpp.o"
+  "CMakeFiles/aslam.dir/src/PangoViewer.cpp.o"
+  "aslam.pdb"
+  "aslam"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/aslam.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
